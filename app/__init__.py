03526b394c9242a2e9bc7bb659e3af493f546ed9
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-18 08:48:14 +0300

    [#161305189] Add the creat_app function in the init file of the app package

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+"""Function to create the app instance"""
+def create_app(test_config=None):
+    app = Flask(__name__,instance_relative_config=True)
+
+    #set the secret key
+    app.config.from_mapping(
+        SECRET_KEY = "Mesh"
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    return app
