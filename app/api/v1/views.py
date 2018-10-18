0b2ae5188b6b00328303b8bbcc77115d7620270e
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-18 15:09:29 +0300

    [#161305189] add code in tests to test json data and assert it

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -2,9 +2,14 @@ from flask import Flask, make_response, jsonify
 from flask_restful import Resource
 
 from app.api.v1.models import products
-class Product(Resource):
+class Products(Resource):
     """Get the list of products in the list"""
     def get(self):
-        return make_response(jsonify({
-            "products" : products
-        }),200)  
+        if len(products)<1:
+            return make_response(jsonify({
+                "Message" : "We dont have any products yet"
+            }))
+        else:
+            return make_response(jsonify({
+                "products" : products
+            }),200)  
--- app/tests/v1/test_storemanager.py
+++ app/tests/v1/test_storemanager.py
@@ -1,5 +1,6 @@
+from flask import json
 import unittest
-import app
+import app, json
 
 class TestStoreApp(unittest.TestCase):
 
@@ -10,5 +11,12 @@ class TestStoreApp(unittest.TestCase):
 
     """ Test whether the application returns and empty list"""
     def test_returns_an_empty_list(self):
-        
+
         self.assertEqual(self.app.get('/api/v1/products').data, b'{"products":[]}\n')
+    def test_whether_returns_status_code_on_products_query(self):
+        self.assertEqual(self.app.get('/api/v1/products').status_code, 200)
+    
+    def test_returns_message_if_no_products(self):
+        response = self.app.get('/api/v1/products')
+        response = json.loads(response.data)
+        assert response['Message'] == "We dont have any products yet"
