c377f3a9d4d8ee0aba918f9d9fda5bb9db2a08a7
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-18 10:31:32 +0300

    [#161305189] Add a route to the v1 init file

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 
+from app.api.v1.views import Product
 v1 = Blueprint('bp',__name__,url_prefix='/api/v1')
 
 app = Api(v1)
 
+app.add_resource(Product,'/products')
