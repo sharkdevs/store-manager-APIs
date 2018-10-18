4c0459c1d715a6ead5758f52cbc9e13ff362cea3
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-18 22:07:38 +0300

    [#161325296] Add a function post in views to create a new product

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,7 +1,7 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify,request
 from flask_restful import Resource
 
-from app.api.v1.models import products
+from app.api.v1.models import Product as p, products
 class Products(Resource):
     """Get the list of products in the list"""
     def get(self):
@@ -12,4 +12,17 @@ class Products(Resource):
         else:
             return make_response(jsonify({
                 "products" : products
-            }),200)  
+            }),200) 
+
+    """Add a new product to the store"""
+    def post(self):
+        data = request.get_json()
+
+        # create one product
+        product = p(data['product_name'],data['product_price'],data['description'],data['quantity'],data['product_image']).create_a_product()
+
+        # add to a list and return it
+        products.append(product)
+        return make_response(jsonify({
+            "products" : products
+        }), 201)
