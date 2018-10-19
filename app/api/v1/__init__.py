b7f87bf2b10d54089ab3a80f09e2c93f0031aed3
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-19 17:24:51 +0300

    [#161342592] register login user api

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 
-from app.api.v1.views import Products, OneProduct, CreateSaleOrder, GetOneSaleRecord, UserRegistration
+from app.api.v1.views import Products, OneProduct, CreateSaleOrder, GetOneSaleRecord, UserRegistration, UserLogin
 v1 = Blueprint('bp',__name__,url_prefix='/api/v1')
 
 app = Api(v1)
@@ -11,3 +11,4 @@ app.add_resource(OneProduct,'/products/<int:id>')
 app.add_resource( CreateSaleOrder,'/sales')
 app.add_resource(GetOneSaleRecord,'/sales/<int:id>')
 app.add_resource(UserRegistration, '/users/registration')
+app.add_resource(UserLogin, '/users/login')
