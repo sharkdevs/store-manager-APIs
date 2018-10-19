bcadf5d51cf8be3bb657c91101db17db532a9373
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-19 08:14:54 +0300

    [#161335277] Register the sale order api

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 
-from app.api.v1.views import Products, OneProduct
+from app.api.v1.views import Products, OneProduct, CreateSaleOrder
 v1 = Blueprint('bp',__name__,url_prefix='/api/v1')
 
 app = Api(v1)
 
 app.add_resource(Products,'/products')
 app.add_resource(OneProduct,'/products/<int:id>')
+app.add_resource( CreateSaleOrder,'/sales')
