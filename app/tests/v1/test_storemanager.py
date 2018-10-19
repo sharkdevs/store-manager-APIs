9b4dc7b404af8d44a08178203d3fa1ac3f977bef
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-19 05:54:16 +0300

    [#161334347] Add a function to test whether a given product is fetched

--- app/tests/v1/test_storemanager.py
+++ app/tests/v1/test_storemanager.py
@@ -2,6 +2,8 @@
 import unittest
 import app, json
 
+from app.api.v1.models import products
+
 class TestStoreApp(unittest.TestCase):
 
     """Setup the test client"""
@@ -19,6 +21,8 @@ class TestStoreApp(unittest.TestCase):
             "product_image" : "image/d.jpg"
         } 
 
+        products.append(self.sample_data)
+        
     def test_whether_returns_status_code_on_products_query(self):
         self.assertEqual(self.app.get('/api/v1/products').status_code, 200)
     
@@ -38,3 +42,8 @@ class TestStoreApp(unittest.TestCase):
     def test_malformed_post_one_product_url(self):
         response = self.app.post('/api/v1/products//', data = json.dumps(self.sample_data), content_type='application/json')
         self.assertEqual(response.status_code,404)
+    
+    '''Test whether the api fetches a product successfull'''
+    def test_get_a_product_by_id(self):
+        response = self.app.get('/api/v1/products/<int:id>')
+        self.assertEqual(response.status_code,200)
