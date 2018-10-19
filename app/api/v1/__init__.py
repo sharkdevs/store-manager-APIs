c0c49328bc86aa1331915868fe7c82c267f4b8fe
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-19 06:05:21 +0300

    [#161334347] Register the API url in the init v1 file

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 
-from app.api.v1.views import Products
+from app.api.v1.views import Products, OneProduct
 v1 = Blueprint('bp',__name__,url_prefix='/api/v1')
 
 app = Api(v1)
 
 app.add_resource(Products,'/products')
+app.add_resource(OneProduct,'/Products/<int:id>')
