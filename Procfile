18d4f7ba0f3b18de983f245709b6c0140b90888b
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-20 23:00:41 +0300

    [#161342592] Add a run.py file and set configurations

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn store-manager-APIs:app
+web: gunicorn run:app
---
+++ run.py
@@ -0,0 +1,7 @@
+import os 
+from app import create_app
+app = create_app("development")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
