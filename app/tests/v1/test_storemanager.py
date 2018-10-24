f430457151c2a5c70b11da5648d8d5c831d7c03e
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-24 16:27:28 +0300

    [Finishes #161447938] Remove API base url importation

--- app/tests/v1/test_storemanager.py
+++ app/tests/v1/test_storemanager.py
@@ -15,8 +15,9 @@ class TestStoreApp(unittest.TestCase):
         test_app = create_app(config_name='testing')
         test_app.app_context().push()
         self.app = test_app.test_client() 
-
-        base_url = '/api/v1'
+        
+        '''This shall be appended to the specific url'''
+        self.base_url = '/api/v1/'
         '''Import all the data to be used for users'''
         self.sample_data = TestData.sample_data
         self.sample_sales_data = TestData.sample_sales_data
@@ -24,7 +25,7 @@ class TestStoreApp(unittest.TestCase):
 
         #register a sample user
         self.app.post(
-            '/api/v1/users/registration', 
+            '{}users/registration'.format(self.base_url), 
             data = json.dumps(self.sample_user), 
             content_type='application/json'
             )
@@ -37,44 +38,44 @@ class TestStoreApp(unittest.TestCase):
 
         
     def test_whether_returns_status_code_on_products_query(self):
-        self.assertEqual(self.app.get('/api/v1/products', headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json').status_code, 200)
+        self.assertEqual(self.app.get('{}products'.format(self.base_url), headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json').status_code, 200)
     
     def test_returns_message_if_no_products(self):
-        response = self.app.get('/api/v1/products', headers = dict(Authorization = "Bearer " + self.auth_token), content_type='application/json')
+        response = self.app.get('{}products'.format(self.base_url), headers = dict(Authorization = "Bearer " + self.auth_token), content_type='application/json')
         assert response.status_code == 200
     
     """Returns 404 if the url is malformed and does not fetch data"""
     def test_malformed_url_on_products_query(self):
-        response = self.app.get('/api/v1/produc', headers = dict(Authorization="Bearer " + self.auth_token))
+        response = self.app.get('{}produc'.format(self.base_url), headers = dict(Authorization="Bearer " + self.auth_token))
         self.assertEqual(response.status_code, 404)
 
     def test_adds_a_new_product(self):
-        response = self.app.post('/api/v1/products',headers = dict(Authorization="Bearer " + self.auth_token), data = json.dumps(self.sample_data), content_type='application/json')
+        response = self.app.post('{}products'.format(self.base_url),headers = dict(Authorization="Bearer " + self.auth_token), data = json.dumps(self.sample_data), content_type='application/json')
         self.assertEqual(response.status_code,201)
 
     def test_malformed_post_one_product_url(self):
-        response = self.app.post('/api/v1/products//', data = json.dumps(self.sample_data), content_type='application/json')
+        response = self.app.post('{}products//'.format(self.base_url), data = json.dumps(self.sample_data), content_type='application/json')
         self.assertEqual(response.status_code,404)
     
     '''Test whether the api fetches a product successfull'''
     def test_get_a_product_by_id(self):
-        response = self.app.get('/api/v1/products/1',headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json')
+        response = self.app.get('{}products/1'.format(self.base_url),headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json')
         self.assertEqual(response.status_code,200)
 
     def test_malformed_url_on_a_given_product_query_by_id(self):
-        response = self.app.get('/api/v1/products/1/wsd')
+        response = self.app.get('{}products/1/wsd'.format(self.base_url))
         self.assertEqual(response.status_code,404)
     
     '''Returns a message if the id is ou of bounds'''
     def test_gives_error_feedback_if_product_id_out_of_bounds(self):
-        response = self.app.get('/api/v1/products/0',headers = dict(Authorization="Bearer " + self.auth_token),content_type='application/json')
+        response = self.app.get('{}products/0'.format(self.base_url),headers = dict(Authorization="Bearer " + self.auth_token),content_type='application/json')
         res = json.loads(response.data)
         self.assertEqual(res['Message'],"The product requested is not in store") 
 
     def test_adds_a_new_sale_order_successfully(self):
         products.append(self.sample_data)
         response = self.app.post(
-            '/api/v1/sales',headers = dict(Authorization="Bearer " + self.auth_token), 
+            '{}sales'.format(self.base_url),headers = dict(Authorization="Bearer " + self.auth_token), 
             data = json.dumps(self.sample_sales_data), 
             content_type='application/json'
             )
@@ -85,7 +86,7 @@ class TestStoreApp(unittest.TestCase):
         products.append(self.sample_data)
         self.sample_sales_data['quantity']=80 #make the quantity more than stock
         feedback = self.app.post(
-            '/api/v1/sales', headers = dict(Authorization="Bearer " + self.auth_token),
+            '{}sales'.format(self.base_url), headers = dict(Authorization="Bearer " + self.auth_token),
             data = json.dumps(self.sample_sales_data), 
             content_type='application/json'
             )
@@ -94,22 +95,22 @@ class TestStoreApp(unittest.TestCase):
 
     """A test to check whether sales are returned successfull"""
     def test_return_all_sales_orders(self):
-        self.assertEqual(self.app.get('/api/v1/sales',headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json').status_code, 200)
+        self.assertEqual(self.app.get('{}sales'.format(self.base_url),headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json').status_code, 200)
     
     '''Test whether a malformed url gives an error'''
     def test_malformed_url_for_get_all_products(self):
-        response = self.app.get('/api/v1/saleswsd')
+        response = self.app.get('{}saleswsd'.format(self.base_url))
         self.assertEqual(response.status_code,404)
 
     '''Test to see whether returns a specific sales order'''
     def test_gets_one_sales_order_by_id(self):
-        response = self.app.get('/api/v1/sales/1', headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json')
+        response = self.app.get('{}sales/1'.format(self.base_url), headers = dict(Authorization="Bearer " + self.auth_token), content_type='application/json')
         self.assertEqual(response.status_code,200)
     
     """Test user registration"""
     def test_adds_a_new_user(self):
-        response = self.app.post('/api/v1/users/registration', data = json.dumps(self.sample_user), content_type='application/json')
+        response = self.app.post('{}users/registration'.format(self.base_url), data = json.dumps(self.sample_user), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     #def test_user_login(self):
-        #self.assertEqual(self.app.post('/api/v1/users/login').status_code, 200)
+        #self.assertEqual(self.app.post('{}users/login'.format(self.base_url)).status_code, 200)
